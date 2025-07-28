870f0c593c1a6d79b3fc6aff22c0de110a6b7d4d
Akshal-r <akshalr920822104009@nprcolleges.org>
2025-07-28 16:04:12 +0530

    updated

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+
+CORS(app, resources={r"/*": {"origins": "https://pibitechcoursepage.vercel.app"}})
 
 client = MongoClient(os.getenv("Mongo_URL"))
 db = client["Studentsdata"]
@@ -17,21 +18,15 @@ users = db["students"]
 def home():
     return "Welcome from backend", 200
 
-@app.route('/data', methods=['GET', 'POST'])
-def first():
-    if request.method == 'GET':
-        return jsonify({"message": "GET request received. Nothing to see here!"}), 200
-
+@app.route('/data', methods=['POST'])
+def submit_data():
     if not request.is_json:
         return jsonify({"error": "Request must be JSON"}), 415
 
     data = request.get_json()
 
-    if "email" not in data:
-        return jsonify({"error": "Missing 'email' in request"}), 400
-
-    if "phone" not in data:
-        return jsonify({"error": "Missing 'phone' in request"}), 400    
+    if "email" not in data or "phone" not in data:
+        return jsonify({"error": "Missing email or phone"}), 400
 
     if users.find_one({"email": data["email"]}) or users.find_one({"phone": data["phone"]}):
         return jsonify({"message": "Message Already Sent"}), 400
@@ -41,4 +36,4 @@ def first():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
