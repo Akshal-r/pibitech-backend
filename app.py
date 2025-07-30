ccde40ff01189ce25f2da7666cfeb0be57a6f018
Akshal-r <akshalr920822104009@nprcolleges.org>
2025-07-30 11:48:58 +0530

    update

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, origins="https://pibitechcoursepage.vercel.app", methods=["GET", "POST", "OPTIONS"], headers=["Content-Type", "Authorization"])
+CORS(app, origins=["https://pibitechcoursepage.vercel.app", "http://localhost:8080"], methods=["GET", "POST", "OPTIONS"], headers=["Content-Type", "Authorization"])
+
 
 try:
     client = MongoClient(os.getenv("Mongo_URL"))
